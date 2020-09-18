9e89233e32d58604edda29201c0cb9559f2baa42
WietseCollaer <wietse.collaer@live.com>
2020-09-18 13:12:23 +0200

    Retrieve meeting rooms

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,21 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {MeetingRoomWebSocketService} from './service/meeting-room-web-socket.service';
+import {MeetingRoom} from './model/meeting-room';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'virtual-meeting-room-frontend';
+export class AppComponent implements OnInit {
+
+  meetingRooms: MeetingRoom[];
+
+  constructor(private meetingRoomWebSocketService: MeetingRoomWebSocketService) {
+  }
+
+  ngOnInit(): void {
+    this.meetingRoomWebSocketService.findAllMeetingRooms().subscribe(meetingRooms => this.meetingRooms = meetingRooms);
+  }
+
 }
---
+++ src/app/model/meeting-room.ts
@@ -0,0 +1,5 @@
+export interface MeetingRoom {
+  id: string;
+  name: string;
+  url: string;
+}
